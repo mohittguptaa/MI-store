be110019aaa1f4b126195c0f239074a8e61c35bf
mohittguptaa <mohitgupta8218@gmail.com>
2024-02-07 21:59:54 +0530

    all st good to go

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,19 +11,19 @@ const Navbar = () => {
             <div className="logo">
                
              <a href="/">
-                  <img id="logoImage" src={logo} alt="Not" /> 
+                  <img id="logoImage" src="https://i01.appmifile.com/webfile/globalimg/pandora/mi-logo.svg" alt="Not" /> 
              </a>
 
              </div>
 
-             <a  className="navlinks" href="/#miphones">Mi Phones</a>
-           <a   className="navlinks"  href="/#redmiphones">Redmi Phones</a>
-           <a   className="navlinks" href="/#tv">TV</a>
-           <a   className="navlinks" href="/#laptops">Laptops</a>
-           <a   className="navlinks" href="/#lifestyle">Fitness & Lifestyle</a>
-           <a  className="navlinks" href="/#home">Home</a>
-           <a   className="navlinks" href="/#audio">Radio</a>
-           <a   className="navlinks" href="/#accessories">Accessories</a>
+             <a  className="navlinks" href="/miphones">Mi Phones</a>
+           <a   className="navlinks"  href="/redmiphones">Redmi Phones</a>
+           <a   className="navlinks" href="/tv">TV</a>
+           <a   className="navlinks" href="/laptops">Laptops</a>
+           <a   className="navlinks" href="/lifestyle">Fitness & Lifestyle</a>
+           <a  className="navlinks" href="/home">Home</a>
+           <a   className="navlinks" href="/audio">Radio</a>
+           <a   className="navlinks" href="/accessories">Accessories</a>
 
            <div className="searchbox">
               <input type="text" name="search"  placeholder="Search Products"/>
